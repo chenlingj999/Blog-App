56505a2926dd06c5717b08d3329a2d2b36ed0f19
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-09-01 19:02:32 -0400

    Finished basic homepage building

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,56 @@ import './App.css';
 
 function App() {
   return (
-    <div>test</div>
+    <main>
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div class="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" />
+        </div>
+        <div class="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">SpongeBob SquarePants</a>
+            <time datetime="">2024-07-21 12:45</time>
+          </p>
+          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div class="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" />
+        </div>
+        <div class="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">SpongeBob SquarePants</a>
+            <time datetime="">2024-07-21 12:45</time>
+          </p>
+          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div class="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" />
+        </div>
+        <div class="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">SpongeBob SquarePants</a>
+            <time datetime="">2024-07-21 12:45</time>
+          </p>
+          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
