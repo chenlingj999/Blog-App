1b6a809acf1f1b486772baaea9caff6632e001d8
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-09-02 19:40:46 -0400

    Implemented the register page and allowed users to register an acc

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://chenlingj999:fsj452qUWOnmyhoQ@cluster0.bxoed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch(error) {
+        res.status(400).json(error);
+    }
+});
+
+app.listen(4000);
