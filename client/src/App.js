f0e31a9831fcba0158b5f8f9e726bc6d095caa82
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-09-01 21:20:37 -0400

    Builded the login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from 'react-router-dom';
 import Layout from './Layout.js';
 import { IndexPage } from './pages/IndexPage.js';
 import LoginPage from './pages/LoginPage.js';
+import RegisterPage from './pages/RegisterPage.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
   );
