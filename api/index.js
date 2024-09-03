cc816d93391ddb0c01f40939f3b0e80e044098b7
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-09-03 17:02:03 -0400

    Displayed posts from database instead of hardcoded ones

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ const secret = 'asd98f76asd789f6asdf0a98sd7fasdf097';
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/upload', express.static(__dirname + '/upload'));
 
 mongoose.connect('mongodb+srv://chenlingj999:fsj452qUWOnmyhoQ@cluster0.bxoed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -70,15 +71,29 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     const newPath = path + '.' + ext;
     fs.renameSync(path, newPath);
 
-    const {title, summary, content} = req.body;
-    const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const {title, summary, content} = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id, 
+        });
+        res.json({postDoc});
     });
+});
 
-    res.json({postDoc});
+app.get('/post', async (req, res) => {
+
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({createdAt: -1})
+            .limit(20)
+    );
 });
 
 app.listen(4000);
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import {formatISO9075} from "date-fns";
+
+export default function Post({title, summary, cover, content, createdAt, author}) {
     return (
         <div className="post">
         <div class="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg" />
+          <img src={'http://localhost:4000/' + cover} alt="" />
         </div>
         <div class="text">
-          <h2>Full-house battery backup coming later this year</h2>
+          <h2>{title}</h2>
           <p className="info">
-            <a className="author">SpongeBob SquarePants</a>
-            <time datetime="">2024-07-21 12:45</time>
+            <a className="author">{author.username}</a>
+            <time>{formatISO9075(new Date(createdAt))}</time>
           </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          <p className="summary">{summary}</p>
         </div>
       </div>
     );
