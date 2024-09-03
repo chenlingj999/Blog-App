2e74c7c00c7792798932f575c4819bb699d665ce
Ling <126978057+chenlingj999@users.noreply.github.com>
2024-09-02 23:08:02 -0400

    Finished setting up login page authentication

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asd98f76asd789f6asdf0a98sd7fasdf097';
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://chenlingj999:fsj452qUWOnmyhoQ@cluster0.bxoed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -22,6 +24,21 @@ app.post('/register', async (req, res) => {
         res.json(userDoc);
     } catch(error) {
         res.status(400).json(error);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
     }
 });
 
